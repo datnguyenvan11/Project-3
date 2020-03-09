9e5393aae98f04413379f890f20be0da81461638
Anh Hoc Dot <anh@hocdot.com>
2020-03-09 22:08:48 +0700

    update template

--- Project-Sem-3/Controllers/HomeController.cs
+++ Project-Sem-3/Controllers/HomeController.cs
@@ -13,5 +13,23 @@ namespace Project_Sem_3.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Insurance()
+        {
+            return View();
+        }
+        public ActionResult Resource()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+       
+
     }
 }
