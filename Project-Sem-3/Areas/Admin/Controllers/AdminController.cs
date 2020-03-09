5f9106cdf7f3d2d33d56c01c911dc858009c7256
luongxuanbach <43542436+luongxuanbach@users.noreply.github.com>
2020-03-09 21:12:24 +0700

    template admin

---
+++ Project-Sem-3/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Sem_3.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Table()
+        {
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+}
